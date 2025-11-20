Bugfix: mi averaging to prevent negative surprise + accumulation

recovar/representation_learning_models.py
@@ -382,7 +382,8 @@ class RepresentationLearningMultipleAutoencoder(keras.Model):
                                         1., 0.)
             mi_single = log_p_cond_comp - log_p_comp
             mi_single_oversampled = mi_single @ oversampler_mask
-            average_surprise = average_surprise + mi_single_oversampled
+            mi_single_oversampled_mean = tf.reduce_mean(mi_single_oversampled, axis=0)
+            average_surprise = average_surprise + mi_single_oversampled_mean
     
         return average_surprise
         