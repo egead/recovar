2f1c0dc6d7ac1c0857157b7dd2ad1e9ba7709587
Ege Adiguzel <egeisxol@gmail.com>
2025-12-19 20:12:24 +0300

    Changed kfold dynamic pipeline batch multiplier handling

--- reproducibility/dynamic_training.py
+++ reproducibility/dynamic_training.py
@@ -2,13 +2,12 @@ from kfold_dynamic_trainer import KfoldDynamicTrainer
 from recovar import RepresentationLearningMultipleAutoencoder
 
 trainer = KfoldDynamicTrainer(
-    exp_name="SILIVRI2019_DYNAMIC_3",
+    exp_name="SILIVRI2019_DYNAMIC_64",
     model_class=RepresentationLearningMultipleAutoencoder,
     dataset="SILIVRI2019",
     split=0,
     epochs=100,
-    batch_multiplier=3,
-    keep_top_batches=0.1, 
+    final_dilation_number=64
 ) 
 
 trainer.train()
--- reproducibility/kfold_dynamic_trainer.py
+++ reproducibility/kfold_dynamic_trainer.py
@@ -16,8 +16,7 @@ class KfoldDynamicTrainer(KfoldTrainer):
         dataset,
         split,
         epochs,
-        batch_multiplier=10,  
-        keep_top_batches=2,        
+        final_dilation_number=64,        
         apply_resampling=False,
         resampling_eq_ratio=0.5,
         resample_while_keeping_total_waveforms_fixed=False,
@@ -41,8 +40,7 @@ class KfoldDynamicTrainer(KfoldTrainer):
             beta_2=beta_2,
         )
 
-        self.batch_multiplier = batch_multiplier
-        self.keep_top_batches = keep_top_batches
+        self.final_dilation_number = final_dilation_number
 
     def train(self):
         kfold_env = KFoldEnvironment(
@@ -93,139 +91,73 @@ class KfoldDynamicTrainer(KfoldTrainer):
 
         self._save_history(self.split, history)
 
+    @tf.function
+    def _train_step(self, model, optimizer, x_train):
+        with tf.GradientTape() as tape:
+            f1, f2, f3, f4, f5, y1, y2, y3, y4, y5 = model(x_train, training=True)
+
+            per_sample_recon_loss = (
+                self._per_sample_l2_distance(x_train, y1) +
+                self._per_sample_l2_distance(x_train, y2) +
+                self._per_sample_l2_distance(x_train, y3) +
+                self._per_sample_l2_distance(x_train, y4) +
+                self._per_sample_l2_distance(x_train, y5)
+            ) / 5.0
+
+            per_sample_ensemble_loss = (
+                self._per_sample_ensemble_distance(f1, f2) +
+                self._per_sample_ensemble_distance(f1, f3) +
+                self._per_sample_ensemble_distance(f2, f3) +
+                self._per_sample_ensemble_distance(f1, f4) +
+                self._per_sample_ensemble_distance(f2, f4) +
+                self._per_sample_ensemble_distance(f3, f4) +
+                self._per_sample_ensemble_distance(f1, f5) +
+                self._per_sample_ensemble_distance(f2, f5) +
+                self._per_sample_ensemble_distance(f3, f5) +
+                self._per_sample_ensemble_distance(f4, f5)
+            ) / 10.0
+
+            per_sample_total_loss = per_sample_recon_loss + per_sample_ensemble_loss
+            loss = tf.reduce_mean(per_sample_total_loss)
+
+        gradients = tape.gradient(loss, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+        return loss
+    
+    def _get_dilation_number(self, epoch):
+        dilation_numbers = np.logspace(np.log2(1.0), np.log2(self.final_dilation_number), base=2, num=self.epochs)
+        return np.round(dilation_numbers[epoch])
+    
     def _train_one_epoch(self, model, optimizer, train_gen, classifier, epoch):
         n_batches = len(train_gen)
         epoch_losses = []
 
-        #Linear interpolation
-        effective_keep_batches = self.batch_multiplier - (epoch / self.epochs) * (self.batch_multiplier - self.keep_top_batches)
+        grouped_batches = []
+        grouped_batch_scores = []
+        
+        for batch_idx in range(n_batches):
+            x_batch, y_batch = train_gen[batch_idx]
+            batch_scores = classifier(x_batch, training=False)
+            
+            grouped_batches.append(x_batch)
+            grouped_batch_scores.append(batch_scores)
+            
+            dilation_number = self._get_dilation_number(epoch)
+                
+            if (batch_idx % dilation_number == 0) and (batch_idx > 0):
+                x_pool = np.concatenate(grouped_batches, axis=0)
+                kept_idxs = np.argsort(batch_scores)[::-1][0:BATCH_SIZE]
+                
+                x_train = x_pool[kept_idxs]
+        
+                loss = self._train_step(model, optimizer, x_train)
+                epoch_losses.append(float(loss))
 
-        print(f"Epoch {epoch}: Loading {self.batch_multiplier} batches, "
-              f"keeping top {effective_keep_batches:.2f} batches ({100*effective_keep_batches/self.batch_multiplier:.1f}%)")
-
-        n_super_batches = n_batches // self.batch_multiplier
-        remaining_batches = n_batches % self.batch_multiplier
-
-        for super_batch_idx in range(n_super_batches):
-            x_batches = []
-            y_batches = []
-
-            for mult_idx in range(self.batch_multiplier):
-                batch_idx = super_batch_idx * self.batch_multiplier + mult_idx
-                x_batch, y_batch = train_gen[batch_idx]
-                x_batches.append(x_batch)
-                y_batches.append(y_batch)
-
-            x_all = np.concatenate(x_batches, axis=0)
-            y_all = np.concatenate(y_batches, axis=0)
-
-            scores = classifier(x_all, training=False)
-
-            # Calculate exact number of samples to keep from float value
-            n_keep = int(effective_keep_batches * BATCH_SIZE)
-            top_indices = np.argsort(scores)[::-1][:n_keep]  #Top n_keep samples
-
-            x_selected = x_all[top_indices]
-            y_selected = y_all[top_indices]
-
-            avg_score = scores.mean()
-            top_score = scores[top_indices].mean()
-
-            with tf.GradientTape() as tape:
-                f1, f2, f3, f4, f5, y1, y2, y3, y4, y5 = model(x_selected, training=True)
-
-                per_sample_recon_loss = (
-                    self._per_sample_l2_distance(x_selected, y1) +
-                    self._per_sample_l2_distance(x_selected, y2) +
-                    self._per_sample_l2_distance(x_selected, y3) +
-                    self._per_sample_l2_distance(x_selected, y4) +
-                    self._per_sample_l2_distance(x_selected, y5)
-                ) / 5.0
-
-                per_sample_ensemble_loss = (
-                    self._per_sample_ensemble_distance(f1, f2) +
-                    self._per_sample_ensemble_distance(f1, f3) +
-                    self._per_sample_ensemble_distance(f2, f3) +
-                    self._per_sample_ensemble_distance(f1, f4) +
-                    self._per_sample_ensemble_distance(f2, f4) +
-                    self._per_sample_ensemble_distance(f3, f4) +
-                    self._per_sample_ensemble_distance(f1, f5) +
-                    self._per_sample_ensemble_distance(f2, f5) +
-                    self._per_sample_ensemble_distance(f3, f5) +
-                    self._per_sample_ensemble_distance(f4, f5)
-                ) / 10.0
-
-                per_sample_total_loss = per_sample_recon_loss + per_sample_ensemble_loss
-                loss = tf.reduce_mean(per_sample_total_loss)
-
-            gradients = tape.gradient(loss, model.trainable_variables)
-            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-
-            epoch_losses.append(float(loss))
-
-            if (super_batch_idx + 1) % 10 == 0:
-
-                print(f"  Super-batch {super_batch_idx+1}/{n_super_batches}, "
-                        f"Loss: {loss:.4f}, "
-                        f"Avg score: {avg_score:.4f}, Top score: {top_score:.4f}, "
-                        f"Keeping: {effective_keep_batches}/{self.batch_multiplier} batches")
-
-        if remaining_batches > 0:
-            x_batches = []
-            y_batches = []
-
-            for mult_idx in range(remaining_batches):
-                batch_idx = n_super_batches * self.batch_multiplier + mult_idx
-                x_batch, y_batch = train_gen[batch_idx]
-                x_batches.append(x_batch)
-                y_batches.append(y_batch)
-
-            x_all = np.concatenate(x_batches, axis=0)
-            y_all = np.concatenate(y_batches, axis=0)
-
-            scores = classifier(x_all, training=False)
-
-            n_keep = int(effective_keep_batches * BATCH_SIZE * remaining_batches / self.batch_multiplier)
-            n_keep = max(1, n_keep) 
-            top_indices = np.argsort(scores)[::-1][:n_keep]
-
-            x_selected = x_all[top_indices]
-            y_selected = y_all[top_indices]
-
-            with tf.GradientTape() as tape:
-                f1, f2, f3, f4, f5, y1, y2, y3, y4, y5 = model(x_selected, training=True)
-
-                per_sample_recon_loss = (
-                    self._per_sample_l2_distance(x_selected, y1) +
-                    self._per_sample_l2_distance(x_selected, y2) +
-                    self._per_sample_l2_distance(x_selected, y3) +
-                    self._per_sample_l2_distance(x_selected, y4) +
-                    self._per_sample_l2_distance(x_selected, y5)
-                ) / 5.0
-
-                per_sample_ensemble_loss = (
-                    self._per_sample_ensemble_distance(f1, f2) +
-                    self._per_sample_ensemble_distance(f1, f3) +
-                    self._per_sample_ensemble_distance(f2, f3) +
-                    self._per_sample_ensemble_distance(f1, f4) +
-                    self._per_sample_ensemble_distance(f2, f4) +
-                    self._per_sample_ensemble_distance(f3, f4) +
-                    self._per_sample_ensemble_distance(f1, f5) +
-                    self._per_sample_ensemble_distance(f2, f5) +
-                    self._per_sample_ensemble_distance(f3, f5) +
-                    self._per_sample_ensemble_distance(f4, f5)
-                ) / 10.0
-
-                per_sample_total_loss = per_sample_recon_loss + per_sample_ensemble_loss
-                loss = tf.reduce_mean(per_sample_total_loss)
-
-            gradients = tape.gradient(loss, model.trainable_variables)
-            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
-
-            epoch_losses.append(float(loss))
-
-            print(f"  Remaining batches: {remaining_batches}, Loss: {loss:.4f}")
-
+                grouped_batches = []
+                grouped_batch_scores = []
+                
+                print(f"Batch Loss: {loss:.4f}")
+                
         return np.mean(epoch_losses)
 
     def _validate(self, model, validation_gen):
