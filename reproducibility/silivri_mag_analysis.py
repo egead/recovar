76e03167200380a925714cb9bf805bfe67b97483
Ege Adıgüzel <egeisxol@gmail.com>
2026-07-18 04:26:28 +0200

    import fix

--- reproducibility/silivri_mag_analysis.py
+++ reproducibility/silivri_mag_analysis.py
@@ -1,7 +1,9 @@
 import os
+import sys
 from pathlib import Path
 
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 
 import matplotlib.pyplot as plt
 import numpy as np
